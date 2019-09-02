172184cc3cfb242de563183efe2022efebfe5f61
Darius Stewart <darius.stewart31@gmail.com>
2019-09-02 15:14:07 +0000

    Automatically backed up by Learn

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -37,7 +37,7 @@ describe './lib/tic_tac_toe.rb' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       num_of_turns = 0
       allow($stdout).to receive(:puts)
-      allow(self).to receive(:gets).and_return("1","2","3")
+      allow(self).to receive(:gets).and_return("9","7","8")
       allow(self).to receive(:over?).and_return(false, false, false, true)
       allow(self).to receive(:turn) do
         num_of_turns += 1
